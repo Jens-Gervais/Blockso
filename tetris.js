250369f62161b053ea90369374517bff89c472fc
Storm <yannic.freson1@gmail.com>
2020-06-17 22:27:26 +0200

    Update

--- tetris.js
+++ tetris.js
@@ -3,8 +3,22 @@ const context = canvas.getContext('2d');
 
 context.scale(20, 20);
 
-context.fillStyle = '#000';
+context.fillStyle = '#99aab5';
 context.fillRect(0, 0, canvas.width, canvas.height);
+
+function boardSweep() {
+    outer: for (let y = board.length - 1; y > 0; y--) {
+        for (let x = 0; x < board[y].length; x++) {
+            if (board[y][x] === 0) {
+                continue outer;
+            }
+        }
+
+        let row = board.splice(y, 1)[0].fill(0);
+        board.unshift(row);
+        y++
+    }
+}
 
 function collide(board, player) {
     const matrix = player.matrix;
@@ -38,38 +52,38 @@ function createPiece(type) {
         ];
     } else if (type === 'I') {
         return [
-            [0, 0, 1, 0],
-            [0, 0, 1, 0],
-            [0, 0, 1, 0],
-            [0, 0, 1, 0]
+            [0, 0, 2, 0],
+            [0, 0, 2, 0],
+            [0, 0, 2, 0],
+            [0, 0, 2, 0]
         ];
     } else if (type === 'J') {
         return [
-            [0, 0, 1],
-            [0, 0, 1],
-            [0, 1, 1]
+            [0, 0, 3],
+            [0, 0, 3],
+            [0, 3, 3]
         ];
     } else if (type === 'L') {
         return [
-            [1, 0, 0],
-            [1, 0, 0],
-            [1, 1, 0]
+            [4, 0, 0],
+            [4, 0, 0],
+            [4, 4, 0]
         ];
     } else if (type === 'O') {
         return [
-            [1, 1],
-            [1, 1]
+            [5, 5],
+            [5, 5]
         ];
     } else if (type === 'S') {
         return [
-            [0, 1, 1],
-            [1, 1, 0],
+            [0, 6, 6],
+            [6, 6, 0],
             [0, 0, 0]
         ];
     } else if (type === 'Z') {
         return [
-            [1, 1, 0],
-            [0, 1, 1],
+            [7, 7, 0],
+            [0, 7, 7],
             [0, 0, 0]
         ];
     }
@@ -79,7 +93,7 @@ function drawMatrix(matrix, offset) {
     matrix.forEach((row, y) => {
         row.forEach((value, x) => {
             if (value !== 0) {
-                context.fillStyle = 'red';
+                context.fillStyle = colors[value];
                 context.fillRect(x + offset.x, y + offset.y, 1, 1);
             }
         });
@@ -87,7 +101,7 @@ function drawMatrix(matrix, offset) {
 }
 
 function draw() {
-    context.fillStyle = '#000';
+    context.fillStyle = '#2c2f33';
     context.fillRect(0, 0, canvas.width, canvas.height);
 
     drawMatrix(board, { x: 0, y: 0 });
@@ -107,18 +121,39 @@ function merge(board, player) {
 function playerDrop() {
     player.pos.y++;
     if (collide(board, player)) {
-        player.pos.y--
+        player.pos.y--;
         merge(board, player);
-        player.pos.y = 0
-        player.pos.x = 5
+        playerReset();
+        boardSweep();
     }
     dropCounter = 0;
+}
+
+function playerInstaDrop() {
+    while (!collide(board, player)) {
+        player.pos.y++
+    }
+    player.pos.y--;
+    merge(board, player);
+    playerReset();
+    boardSweep();
 }
 
 function playerMove(direction) {
     player.pos.x += direction;
     if (collide(board, player)) {
         player.pos.x -= direction;
+    }
+}
+
+function playerReset() {
+    const pieces = ['T', 'I', 'J', 'L', 'O', 'S', 'Z'];
+    player.matrix = createPiece(pieces[Math.floor(pieces.length * Math.random())])
+    player.pos.y = 0;
+    player.pos.x = Math.floor(board[0].length / 2) - 1;
+
+    if (collide(board, player)) {
+        board.forEach(row => row.fill(0))
     }
 }
 
@@ -143,23 +178,10 @@ function rotate(matrix) {
     matrix.forEach(row => row.reverse());
 }
 
-/* function rotate(matrix, direction) {
-    for (let y = 0; y < matrix.length, y++) {
-        for (let x = 0; x < y; x++) {
-            [matrix[x][y], matrix[y][x]] = [matrix[y][x], matrix[x][y]]
-        }
-    }
-
-    if (direction > 0) {
-        matrix.forEach(row => row.reverse());
-    } else {
-        matrix.reverse();
-    }
-} */
-
 let lastTime = 0;
 let dropCounter = 0;
 let dropInterval = 1000;
+
 function update(time = 0) {
     let deltaTime = time - lastTime;
 
@@ -173,6 +195,8 @@ function update(time = 0) {
     draw();
     requestAnimationFrame(update);
 }
+
+const colors = [null, '#2fedfd', '#5fd2ff', '#969efd', '#c465ff', '#e92fff', '#ef16fd', '#e91ff7'];
 
 const board = createMatrix(12, 20);
 
@@ -189,7 +213,9 @@ document.addEventListener('keydown', key => {
     } else if (key.keyCode === 40) {
         playerDrop();
     } else if (key.keyCode === 38) {
-        playerRotate()
+        playerRotate();
+    } else if (key.keyCode === 32) {
+        playerInstaDrop();
     }
 })
 
